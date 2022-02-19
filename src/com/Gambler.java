1a655cae882d159f12cec9b1a069614a7e6043e9
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-19 09:16:54 +0530

    bet() is add to check if gambler win or lose the current bet

--- src/com/Gambler.java
+++ src/com/Gambler.java
@@ -1,8 +1,32 @@
 package com;
 
+import java.util.Scanner;
+
 public class Gambler {
-	Gambler(int betAmount, int startingAmount){
-		int initialBetAmount = betAmount;
-		int startingBetAmount = startingAmount;
+	int stakeAmount;
+	int startingBetAmount;
+	
+	Gambler(){
+		/**
+		 * Constructor to read user stake amount and default bet amount*/
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter an amount to start with: ");
+		stakeAmount = input.nextInt();
+		System.out.print("Please enter an amount to bet every match: ");
+		startingBetAmount = input.nextInt();
+		input.close();
+	}
+	
+	void bet() {
+		double isBetWon = Math.floor((Math.random() * 10) % 2);
+		System.out.println(isBetWon);
+		if(isBetWon == 1) {
+			System.out.println("Player Won the round.");
+			stakeAmount += 1;
+			
+		} else if (isBetWon == 0) {
+			System.out.println("Player Lost the round.");
+			stakeAmount -= 1;
+		}
 	}
 }
