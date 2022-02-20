f09808505e70e7680cc458f61eff04f9f155563d
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-20 20:14:42 +0530

    UC 5, 6 & 7 and done together and updated on this branch

--- src/com/Gambler.java
+++ src/com/Gambler.java
@@ -3,30 +3,32 @@ package com;
 import java.util.Scanner;
 
 public class Gambler {
-	
-	
-	Gambler(){
-		/**
-		 * Constructor to read user stake amount and default bet amount*/
-		Scanner input = new Scanner(System.in);
-		System.out.print("Please enter number of days you want to play: ");
-		numberOfDays = input.nextInt();
-		System.out.print("Please enter an amount to start with: ");
-		stakeAmount = input.nextInt();
-		System.out.print("Please enter an amount to bet every match: ");
-		startingBetAmount = input.nextInt();
-		input.close();
-	}
-	
+
 	int stakeAmount;
 	int numberOfDays;
 	int startingBetAmount;
 	int initialStakeAmount;
 	int maximumWinningAmount;
 	int maximumLosingAmount;
+	int currentMonthWinning;
+	int currentMonthLost;
+	
+	Gambler(){
+		/**
+		 * Constructor to read user stake amount and default bet amount*/
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter number of days you want to play for a month: ");
+		numberOfDays = input.nextInt();
+		System.out.print("Please enter an amount to start with every day: ");
+		initialStakeAmount = input.nextInt();
+		System.out.print("Please enter an amount to bet every match: ");
+		startingBetAmount = input.nextInt();
+		input.close();
+	}
+	
 	
 	public void initiateBetting() {
-		initialStakeAmount = stakeAmount;
+		stakeAmount = initialStakeAmount;
 		maximumWinningAmount = initialStakeAmount + (initialStakeAmount/2);
 		maximumLosingAmount = initialStakeAmount - (initialStakeAmount/2);
 	}
@@ -73,5 +75,49 @@ public class Gambler {
 		}
 		
 		return true;
+	}
+	
+	void calculateCurrentMonthStake(int amount, int dayResult){
+		if(dayResult == 0)
+			currentMonthLost = currentMonthLost + amount;
+		else if (dayResult == 1)
+			currentMonthWinning = currentMonthWinning + amount;
+	}
+	
+	/**
+	 * player will play till the maximum days he enters
+	 */
+	void startPlayingThisMonth() {
+		currentMonthWinning = 0;
+		currentMonthLost = 0;
+		
+		for(int i = 1; i <= numberOfDays; i++) {
+			
+			initiateBetting();
+	
+			boolean isGamblerLeft = gamblerLeavingStage();
+	
+			if(isGamblerLeft) {
+				System.out.println("Day "+i+": Lost today. Current Stake: Rs. " + stakeAmount);
+				calculateCurrentMonthStake(stakeAmount, 0);
+			} else {
+				System.out.println("Day "+i+": Won today. Current Stake Rs. " + stakeAmount);
+				calculateCurrentMonthStake(stakeAmount - initialStakeAmount, 1);
+			}
+			
+		}
+
+		System.out.println("Total amount won this month is Rs. " + (currentMonthWinning));
+		System.out.println("Total amount lost this month is Rs. " + (currentMonthLost));
+		
+		if(currentMonthWinning > currentMonthLost) {
+			System.out.println("As we played well this month, let's continue for another month.");
+			startPlayingThisMonth();
+		} else if(currentMonthWinning == currentMonthLost){
+			System.out.println("Nothing bad, Nothing good. We can still play another month.");
+			startPlayingThisMonth();
+		} else {
+			System.out.println("It's been a terrible month and faced a loss, let's pause betting for next month.");
+		}
 	}
 }
--- src/com/Main.java
+++ src/com/Main.java
@@ -5,23 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		/*new gambler object is created */
 			
-		Gambler gambler = new Gambler();
-		/**
-		 * player will play till the maximum days he enters
-		 */
-		for(int i = 1; i <= gambler.numberOfDays; i++) {
-			
-			gambler.initiateBetting();
-	
-			boolean isGamblerLeft = gambler.gamblerLeavingStage();
-	
-			if(isGamblerLeft) {
-				System.out.println("Day "+i+": Gambler Lost today :(");
-			} else {
-				System.out.println("Day "+i+": Gambler Won today :)");
-			}
-			
-		}
+		Gambler gambler = new Gambler();	
+		gambler.startPlayingThisMonth();
 	}
 
 }
